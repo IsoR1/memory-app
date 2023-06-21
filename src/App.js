e0f76bc59e8bb8dfc18684022ce5b7f28e84697c
Braco <isovicramiz07@gmail.com>
2023-06-21 14:01:52 -0400

    High score is set and tracked, and game is reset when a repeat image is clicked

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 // import logo from "./logo.svg";
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header.js";
@@ -17,21 +18,32 @@ function App() {
     setSelectedDifficulty(difficulty);
   };
 
-  const checkIfHighScore = () => {
-    if (score > highScore) {
-      return true;
-    }
-  };
-
   useEffect(() => {
+    const gameOver = () => {
+      setMoves([]);
+      setScore(0);
+      console.log(moves);
+      return;
+    };
     let targetedNode;
     const handleClick = (e) => {
       const targetedNode = e.target.closest(".card");
       if (!targetedNode) {
         return;
       }
+      if (moves.includes(targetedNode.dataset.id)) {
+        gameOver();
+        return;
+      }
       console.log(targetedNode);
+      setMoves([...moves, targetedNode.dataset.id]);
       setScore(score + 1);
+      console.log(moves);
+
+      // score >= highScore ? setHighScore(score + 1) : false;
+      if (score + 1 >= highScore) {
+        setHighScore(score + 1);
+      }
     };
 
     document.addEventListener("click", handleClick);
@@ -39,7 +51,7 @@ function App() {
     return () => {
       document.removeEventListener("click", handleClick);
     };
-  }, [score]);
+  }, [score, highScore]);
 
   return (
     <div className="container">
