98e3f23b92e8da7e0eee56c493ee81d2ac14f724
Braco <isovicramiz07@gmail.com>
2023-06-23 18:35:02 -0400

    Cards flip on hover and show back

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,40 +1,26 @@
-import React, { useState } from "react";
-import uniqid from "uniqid";
-import daemonJpg from "../images/daemon-laura.jpg";
-import jaehaerysJpg from "../images/jaehaerys-one-laura.jpg";
-import maegorJpg from "../images/maegor-laura.jpg";
-import baelorJpg from "../images/baelor-laura.jpg";
+import React, { useEffect, useState } from "react";
+import banner from "../bg_image/targaryen-banner.svg";
 
-function Body({ difficulty }) {
-  let images;
-  if (difficulty === "easy") {
-    images = [
-      { name: "Daemon", path: daemonJpg },
-      { name: "Jaehaerys", path: jaehaerysJpg },
-      { name: "Maegor", path: maegorJpg },
-      { name: "Baelor", path: baelorJpg },
-    ];
-  } else if (difficulty === "hard") {
-    images = [{ name: "Daemon", path: daemonJpg }];
-  } else if (difficulty === "hotd") {
-    images = [{ name: "Baelor", path: baelorJpg }];
+function Body({ difficulty, images, flip }) {
+  if (!images) {
+    return null;
   }
+
   return (
     <div className="card-container">
-      {/* <div
-      className="img-cards"
-      style={{ gridTemplateColumns: `repeat(${images.length}, 1fr)` }}
-    > */}
       {images.map((img) => (
-        <div className="card">
-          <img src={img.path} className="image" />
-          {/* <div className="card-p-div"> */}
-          {/* <p className="card-p">{img.name}</p> */}
-          <span style={{ alignSelf: "center" }}>{img.name}</span>
-          {/* </div> */}
+        <div className={`card ${flip ? "flip" : ""}`} data-id={img.id}>
+          <div className="card-front">
+            <img src={img.path.default} className="image" alt={img.name} />
+            <span className="img-span">
+              <strong className="img-strong">{img.name}</strong>
+            </span>
+          </div>
+          <div className="card-back">
+            <img src={banner} className="back-image" />
+          </div>
         </div>
       ))}
-      {/* </div> */}
     </div>
   );
 }
